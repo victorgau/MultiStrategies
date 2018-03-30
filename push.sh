e06079769effebfa66d6430cae627b2bdc58eba6
Victor Gau <victorgau@gmail.com>
2018-03-31 07:33:05 +0800

    testing travis

--- push.sh
+++ push.sh
@@ -4,5 +4,6 @@ git config --global user.email "victorgau@gmail.com"
 git config --global user.name "Victor Gau"
 git add docs/*.html
 git commit -m "Upload from Travis"
-git remote add origin https://${GH_TOKEN}@github.com/victorgau/MultiStrategies.git > /dev/null 2>&1
-git push --quiet origin master 
+# git remote add origin https://${GH_TOKEN}@github.com/victorgau/MultiStrategies.git > /dev/null 2>&1
+git remote add origin https://github.com/victorgau/MultiStrategies.git
+git push --quiet origin master
